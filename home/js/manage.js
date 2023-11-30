6a57c87e497f16964e8f915a69459c64808e8216
cuongnv108 <laclac10921@gmail.com>
2023-11-30 21:26:03 +0700

    fix

--- home/js/manage.js
+++ home/js/manage.js
@@ -1,16 +1,11 @@
-const container = document.querySelector("#myUlLi");
-
 const role = localStorage.getItem("role")
 const id = localStorage.getItem("id")
-const data = ''
-const getButtonManage = async () => {
-    try {
-        if(role == "doctor"){
-            data = '<li><a href="manage.html?id=${id}">Quản lí lịch</a></li>'
-        }
-        container.innerHTML = data;
-    } catch (error) {
-        console.log(error);
+
+const getButtonManage = () => {
+    const container = document.getElementsByClassName('myUlLi')
+    if(role == "doctor"){
+        const data = '<li><a href="manage.html?id=${id}">Quản lí lịch</a></li>'
+        container.innerHTML = data  
     }
 };
 
