da9ffa6a6ff3c18fefceea394a6071ca51d552f6
cuongnv108 <laclac10921@gmail.com>
2023-11-30 21:47:55 +0700

    fix cứng manage

--- home/js/manage.js
+++ home/js/manage.js
@@ -1,13 +1,17 @@
-const role = localStorage.getItem("role")
-const id = localStorage.getItem("id")
+const role = localStorage.getItem("role");
+const login = localStorage.getItem("login");
+const id = localStorage.getItem("id");
 
 const getButtonManage = () => {
-    const container = document.getElementsByClassName('myUlLi')
-    if(role == "doctor"){
-        const data = '<li><a href="manage.html?id=${id}">Quản lí lịch</a></li>'
-        container.innerHTML = data  
-    }
+	const container = document.querySelector(".myUlLi");
+	if (role === "doctor" && login) {
+		const link = document.createElement("a");
+		link.href = `manage.html?id=${id}`;
+		link.textContent = "Quản lí lịch";
+		const listItem = document.createElement("li");
+		listItem.appendChild(link);
+		container.appendChild(listItem);
+	}
 };
 
 getButtonManage();
-
